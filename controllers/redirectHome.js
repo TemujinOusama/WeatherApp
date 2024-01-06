ca0b6150d54e149147218fd4acddaa1136399ec7
Lariwell <lariwellrodrigueza@gmail.com>
2024-01-06 17:35:53 +0800

    Managed Endpoints

---
+++ controllers/redirectHome.js
@@ -0,0 +1,3 @@
+export const redirectHome = (req, res) => {
+  res.redirect("/home");
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
 import { getHome, postHome } from "../controllers/homeController.js";
+import { redirectHome } from "../controllers/redirectHome.js";
 
 router.use(express.static("public"));
 router.use(express.json());
 
 router.get("/home", getHome);
 router.post("/home", postHome);
-
+router.get("*", redirectHome);
 export default router;
